7bf159ed8f52109d538b9f7ff0fca4b644219afd
Lylia <pro.lylia@gmail.com>
2024-12-04 17:55:59 +0100

    Projet Site Nos formation début

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("contactForm").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+}
+
+
